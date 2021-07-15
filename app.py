a305a4654721a56ab9540bfe36418810d6f9577d
M. Tostes <matheus_tostes@hotmail.com>
2021-07-15 20:49:22 -0300

    Default mode set to P2P for TVBOX

--- app.py
+++ app.py
@@ -227,6 +227,13 @@ def definirUltCliente(ultimoCliente, i):
     ultimoCliente = i
     return ultimoCliente
 
+def alterarListaP2P(driver):
+    driver.find_element_by_xpath('/html/body/div[2]/div/div[3]/button[1]').click()
+    time.sleep(2)
+    driver.find_element_by_xpath('//*[@id="datatabled"]/tbody/tr[1]/td[10]/div/button[2]/i').click()
+    time.sleep(2)
+    driver.find_element_by_xpath('//*[@id="datatabled"]/tbody/tr[1]/td[10]/div/div/a[2]').click()
+
 def application(ultimoCliente):
     try:
         print("Iniciando o bot")
@@ -370,6 +377,8 @@ def application(ultimoCliente):
                                 time.sleep(3)
                             except:
                                 print("Erro ao enviar mensagem Padrao")
+                            
+                            alterarListaP2P(driver)
 
                         if dfClientes.iloc[i][colAparelho] == 'Chromecast':                       
                             aplicativo = 'GSE IPTV'
